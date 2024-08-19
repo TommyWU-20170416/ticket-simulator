f6bb011e2075893bacaa1c361f422c350d125e18
TommyWU-20170416 <tommy111811@gmail.com>
2024-08-19 13:41:12 +0800

    [20240819] backend: initialize project

---
+++ ticket-simulator-services/src/test/java/com/wsi/ticketsimulatorservices/TicketSimulatorServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsi.ticketsimulatorservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSimulatorServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
