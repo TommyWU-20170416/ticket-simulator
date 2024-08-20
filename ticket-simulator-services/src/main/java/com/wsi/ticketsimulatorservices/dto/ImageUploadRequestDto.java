62f5eaafa75e49a80e0b30f38c2bdd0de3364905
TommyWU-20170416 <tommy111811@gmail.com>
2024-08-20 23:23:43 +0800

    [20240820] backend: 調整專案架構，讓 ticket-services 可以獨立出來

--- ticket-simulator-services/src/main/java/com/wsi/ticketsimulatorservices/dto/ImageUploadRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.wsi.ticketsimulatorservices.dto;
-
-import lombok.Data;
-
-@Data
-public class ImageUploadRequestDto {
-    private Integer restaurantId;
-    private String imageBase64;
-}
--- ticket-simulator-services/src/main/java/com/wsi/ticketsimulatorservices/repository/StationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wsi.ticketsimulatorservices.repository;
-
-import com.wsi.ticketsimulatorservices.model.Station;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationRepository extends JpaRepository<Station, Integer> {
-}
