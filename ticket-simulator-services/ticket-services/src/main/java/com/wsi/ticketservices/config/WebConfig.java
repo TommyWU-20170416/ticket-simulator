cf215df6898f6dbe0384d4b46a6341730296e42c
TommyWU-20170416 <tommy111811@gmail.com>
2024-08-21 20:46:34 +0800

    [20240821] backend: 新增 gRPC 並且調整前端為 3000 port

--- ticket-simulator-services/ticket-services/src/main/java/com/wsi/ticketservices/config/WebConfig.java
+++ ticket-simulator-services/ticket-services/src/main/java/com/wsi/ticketservices/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.wsi.travelservices.config;
+package com.wsi.ticketservices.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
