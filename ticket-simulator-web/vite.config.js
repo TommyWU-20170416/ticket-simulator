7798db754aa14bd6fef7211b33170dde1e70d795
TommyWU-20170416 <tommy111811@gmail.com>
2024-08-21 20:52:15 +0800

    [20240821] frontend: 設定 CORS、gprc-client、測試頁面

--- ticket-simulator-web/vite.config.js
+++ ticket-simulator-web/vite.config.js
@@ -3,13 +3,49 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import requireTransform from 'vite-plugin-require-transform'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    requireTransform({
+      fileRegex: /.js$|.vue$/
+    })
   ],
+  server: {
+    port: 3000,
+    /** 如果 gRPC 不能使用就要來設定 */
+    proxy: {
+      '/grpc': {
+        target: 'http://localhost:8079',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/grpc/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            // 添加 CORS 頭部
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            res.setHeader(
+              'Access-Control-Allow-Headers',
+              'X-Requested-With, content-type, Authorization'
+            )
+          })
+        },
+        onProxyReq: (proxyReq, req, res) => {
+          if (req.method === 'OPTIONS') {
+            res.writeHead(200, {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+              'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+            })
+            res.end()
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
