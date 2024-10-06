e3940c8dca3ace93d02c6a9dcbd414f0c6b2538d
TommyWU-20170416 <tommy111811@gmail.com>
2024-10-06 10:38:36 +0800

    [20241006] frontend CI: 更新 api.js 避免 cors

--- ticket-simulator-web/src/api.js
+++ ticket-simulator-web/src/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const travelServices = axios.create({
-  baseURL: 'http://localhost:8541/api/travelServices'
+  baseURL: '/api/travelServices'
   // 你可以在這裡添加更多的配置，例如 headers
 })
 
 export const ticketServices = axios.create({
-  baseURL: 'http://localhost:8551/api/ticketServices'
+  baseURL: '/api/ticketServices'
   // 你可以在這裡添加更多的配置，例如 headers
 })
 
